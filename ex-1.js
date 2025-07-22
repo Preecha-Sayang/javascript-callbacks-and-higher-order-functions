c0aca393b0bc0209d943dda3eb973e0f7a1d49ae
PTAE43 <kritsada1696@gmail.com>
2025-07-22 15:00:35 +0700

    Done:Assignment : JavaScript Callbacks and Higher Order Functions Assignment by ptae43

--- ex-1.js
+++ ex-1.js
@@ -3,15 +3,20 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary + 5000;
 }
 
 function forEach(array, operation) {
   // Start coding here
+  for(let i=0; i<array.length; i++){
+    let currentSalary = array[i];
+    currentSalary = operation(currentSalary);
+  }
+  return array.map(operation); // Return a new array with the results of applying the operation to each element
 }
 
 // Using `forEach` function here
-
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
@@ -19,8 +24,13 @@ console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+Answer: จุดสังเกตุตรง forEach(employeeSalaries, addSalary5000); 
+เพราะ addSalary5000 มีการส่ง Argument เข้าไปในฟังก์ชัน forEach 
+เพื่อใช้ในการดำเนินการกับแต่ละสมาชิกของอาร์เรย์ employeeSalaries
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+Answer: จุดสังเกตุตรง forEach(array, operation) 
+เพราะ forEach เป็นฟังก์ชันที่รับ Argument จากฟังก์ชันอื่น
 
 ====================================
 */
--- ex-2.js
+++ ex-2.js
@@ -5,16 +5,24 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
   // Start coding here
+  return score >= 70 ? true : false;
 }
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for(let i=0; i<array.length; i++){
+    if(operation(array[i])){
+      count++;
+    }
+  }
+  return count >= 5;
 }
 
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,checkGradeOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,checkGradeOver70);
 
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
@@ -24,8 +32,13 @@ console.log(scoreRoom3Result); //false
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+Answer: จุดสังเกตุตรง atLeastFive(studentScoresRoom1, checkGradeOver70);
+เพราะ checkGradeOver70 มีการส่ง Argument เข้าไปในฟังก์ชัน atLeastFive
+เพื่อใช้ในการตรวจสอบคะแนนของนักเรียนแต่ละคน
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+Answer: จุดสังเกตุตรง atLeastFive(array, operation)
+เพราะ atLeastFive เป็นฟังก์ชันที่รับ Argument จากฟังก์ชันอื่น
 
 ====================================
 */
